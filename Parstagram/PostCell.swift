e2ea9547c3855b14ddfc5bdcb3dcaa22d663121e
Ryan Johnson <rnjohnson18@users.noreply.github.com>
2021-10-10 17:32:40 -0500

    Completed required user stories

---
+++ Parstagram/PostCell.swift
@@ -0,0 +1,23 @@
+//
+//  PostCell.swift
+//  Parstagram
+//
+//  Created by Ryan Johnson on 10/10/21.
+//
+
+import UIKit
+
+class PostCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
